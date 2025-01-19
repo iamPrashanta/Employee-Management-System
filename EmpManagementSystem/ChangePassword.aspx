431d008a288428ad90929f038e6a7aaad263fa86
prashantaunpay <prashanta@unpay.in>
2025-01-19 22:50:09 +0530

    add change password code

--- EmpManagementSystem/ChangePassword.aspx
+++ EmpManagementSystem/ChangePassword.aspx
@@ -4,11 +4,29 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <title>Change Password</title>
 </head>
 <body>
-    <form id="form1" runat="server">
+    <form id="changePasswordFrom" runat="server">
         <div>
+            <h2>Change Password</h2>
+
+            <asp:Label ID="lblOldPassword" runat="server" Text="Old Password:"></asp:Label>
+            <asp:TextBox ID="txtOldPassword" runat="server" TextMode="Password"></asp:TextBox>
+            <br /><br />
+
+            <asp:Label ID="lblNewPassword" runat="server" Text="New Password:"></asp:Label>
+            <asp:TextBox ID="txtNewPassword" runat="server" TextMode="Password"></asp:TextBox>
+            <br /><br />
+
+            <asp:Label ID="lblConfirmPassword" runat="server" Text="Confirm Password:"></asp:Label>
+            <asp:TextBox ID="txtConfirmPassword" runat="server" TextMode="Password"></asp:TextBox>
+            <br /><br />
+
+            <asp:Button ID="btnChangePassword" runat="server" Text="Change Password" OnClick="btnChangePassword_Click" />
+            <br /><br />
+
+            <asp:Label ID="lblResult" runat="server" ForeColor="Red"></asp:Label>
         </div>
     </form>
 </body>
--- EmpManagementSystem/ChangePassword.aspx.cs
+++ EmpManagementSystem/ChangePassword.aspx.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.SqlServer.Server;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +14,115 @@ namespace EmpManagementSystem
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["username"] == null || string.IsNullOrEmpty(Session["username"].ToString()))
+            {
+                Response.Redirect("Login01.aspx");
+            }
+            else
+            {
+                String username = Session["username"].ToString();
+                System.Diagnostics.Debug.WriteLine("Session username: " + Session["username"]);
+                if (!IsPostBack)
+                {
+                    // do something
+                }
+            }
+        }
 
+        protected void btnChangePassword_Click(object sender, EventArgs e)
+        {
+            string username = Session["username"].ToString();
+            string oldPassword = txtOldPassword.Text.Trim();
+            string newPassword = txtNewPassword.Text.Trim();
+            string confirmPassword = txtConfirmPassword.Text.Trim();
+
+            if (string.IsNullOrEmpty(oldPassword) || string.IsNullOrEmpty(newPassword) || string.IsNullOrEmpty(confirmPassword))
+            {
+                lblResult.Text = "All fields are required.";
+                return;
+            }
+
+            if (newPassword != confirmPassword)
+            {
+                lblResult.Text = "New Password and Confirm Password do not match.";
+                return;
+            }
+
+            string connectionString = ConfigurationManager.ConnectionStrings["conn"].ToString();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string checkPasswordQuery = "SELECT passwd FROM users WHERE username = @Username AND passwd = @OldPassword";
+                string updatePasswordQuery = "UPDATE users SET passwd = @NewPassword WHERE username = @Username";
+
+                try
+                {
+                    conn.Open();
+
+                    // Check if old password is correct
+                    using (SqlCommand checkCommand = new SqlCommand(checkPasswordQuery, conn))
+                    {
+                        checkCommand.Parameters.AddWithValue("@Username", username);
+                        checkCommand.Parameters.AddWithValue("@OldPassword", oldPassword);
+
+                        object result = checkCommand.ExecuteScalar();
+
+                        if (result == null)
+                        {
+                            lblResult.Text = "Old password is incorrect.";
+                            return;
+                        }
+                    }
+
+                    // Update to new password
+                    using (SqlCommand updateCommand = new SqlCommand(updatePasswordQuery, conn))
+                    {
+                        updateCommand.Parameters.AddWithValue("@Username", username);
+                        updateCommand.Parameters.AddWithValue("@NewPassword", newPassword);
+
+                        int rowsAffected = updateCommand.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            lblResult.Text = "Password changed successfully.";
+                            lblResult.ForeColor = System.Drawing.Color.Green;
+
+                            // Clear the session
+                            Session.Clear();
+                            Session.Abandon();
+
+                            // Hide fields
+                            txtOldPassword.Visible = false;
+                            txtNewPassword.Visible = false;
+                            txtConfirmPassword.Visible = false;
+                            btnChangePassword.Visible = false;
+                            lblOldPassword.Visible = false;
+                            lblNewPassword.Visible = false;
+                            lblConfirmPassword.Visible = false;
+
+                            // Show "Login Again" link
+                            lblResult.Text += " Please log in again.";
+                            HyperLink loginLink = new HyperLink
+                            {
+                                Text = "Login Again",
+                                NavigateUrl = "Login01.aspx"
+                            };
+                            changePasswordFrom.Controls.Add(loginLink);
+                        }
+                        else
+                        {
+                            lblResult.Text = "Failed to change password. Please try again.";
+                        }
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    lblResult.Text = $"Database error: {ex.Message}";
+                }
+                catch (Exception ex)
+                {
+                    lblResult.Text = $"An unexpected error occurred: {ex.Message}";
+                }
+            }
         }
     }
 }
